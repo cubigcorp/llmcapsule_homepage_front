e40a1c7cc28368d88c40cb2ba229ed544f704273
inhwa <inhwa@cubig.ai>
2025-07-28 09:53:27 +0900

    feat: 자동 배포를 위한 설정

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
+  basePath:
+    process.env.NODE_ENV === 'production' ? '/llmcapsule_homepage_front' : '',
+  assetPrefix:
+    process.env.NODE_ENV === 'production' ? '/llmcapsule_homepage_front' : '',
+  trailingSlash: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
