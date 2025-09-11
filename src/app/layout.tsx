53272db9c879551d285bd2f73cf8b3a87dce9b84
정인화 <jung-inhwa@MacBook-Air-3.local>
2025-09-11 13:23:44 +0900

    fix: gtm script 교체 및 facebook 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,21 +28,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5ZBPV3MZ');`,
-              }}
-            />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '995103179338191');
-fbq('track', 'PageView');`,
+})(window,document,'script','dataLayer','GTM-5XXVWKSB');`,
               }}
             />
             <noscript>
@@ -70,7 +56,7 @@ fbq('track', 'PageView');`,
         {isProduction && (
           <noscript>
             <iframe
-              src='https://www.googletagmanager.com/ns.html?id=GTM-5ZBPV3MZ'
+              src='https://www.googletagmanager.com/ns.html?id=GTM-5XXVWKSB'
               height='0'
               width='0'
               style={{ display: 'none', visibility: 'hidden' }}
